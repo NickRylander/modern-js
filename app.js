d5e939cb3ed9407fefae19ec5d969a720a954e7f
NickRylander <nick.rylander@outlook.com>
2022-07-19 17:13:27 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+    // Setting the variable name to a prompt on the server
+let name_input = prompt("What is your name?");
+    // Collecting that input from the div with and id 'welcome' and putting that value into the div, and printing 'Welcome, *name input*!'
+if (name_input != "") {
+    document.getElementById('welcome').innerHTML = `Welcome, ${name_input}!`;
+} else {
+    window.alert("Please refresh and put in a valid name!")
+}
